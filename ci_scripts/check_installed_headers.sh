1df559dfe8bafd3b4997c5dfb0b50ce38bd55216
Seb James <sebj_coder3@pm.me>
2025-06-26 11:30:33 +0100

    Enable check installed headers, also install a few fogotten files

--- ci_scripts/check_installed_headers.sh
+++ ci_scripts/check_installed_headers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check sm headers (Seb's maths library)
-sm_headers=$(find ../sm -type f | grep -v "fonts" | rev | cut -d"/" -f1 | rev | grep "\.h")
-installed_headers=$(grep "\.h" install_manifest.txt | rev | cut -d"/" -f1 | rev)
+# Check sm headers (Seb's maths library). Run this from build/ after make install
+sm_headers=$(find ../sm -type f | grep -v "CMakeLists.txt" | rev | cut -d"/" -f1 | rev)
+installed_headers=$(cat install_manifest.txt | rev | cut -d"/" -f1 | rev)
 missing_headers=$(echo $sm_headers | tr ' ' '\n' | grep -Fxv -f <(echo $installed_headers | tr ' ' '\n'))
 
 if [[ -z "$missing_headers" ]]; then
