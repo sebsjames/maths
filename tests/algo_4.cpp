5ae1e1d05edfea1b9691b2682d2de90d58458697
Seb James <sebj_coder3@pm.me>
2025-11-10 11:55:41 +0000

    Make the test tets

--- tests/algo_4.cpp
+++ tests/algo_4.cpp
@@ -16,27 +16,39 @@ int main()
 
     float d = sm::algo::dist_to_lineseg (t0, t1, p);
     std::cout << "Distance from " << p << " to " << t0 << "-" << t1 << " = " << d << std::endl;
+    if (d != 0.1) { --rtn; }
 
     p = {0.5, 0.0001, 0.002};
     d = sm::algo::dist_to_lineseg (t0, t1, p);
     std::cout << "Distance from " << p << " to " << t0 << "-" << t1 << " = " << d << std::endl;
+    if (d != 0.0020025) { --rtn; }
+
 
     sm::vec<float> t2 = {0,1,0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     float d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
+    if (d != 0.0020025f || d2 != 4.01e-6f) { --rtn; }
+
     p = {0, 0.9, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
+    if (d != 0.0f || d2 != 0.0f) { --rtn; }
+
     p = {0, 0.8, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
+    if (d != 0.0f || d2 != 0.0f) { --rtn; }
+
     p = {0, 1.1, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
+    if (d != 0.1f || d2 != 0.01f) { --rtn; }
+
+    std::cout << "Test " << (rtn ? "success" : "failure") << std::endl;
 
     return rtn;
 }
