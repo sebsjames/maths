1adb2d03c2cb06454b9ad3ef8ade13905235c01a
Seb James <sebj_coder3@pm.me>
2025-11-12 11:53:54 +0000

    Corrected numbers for test

--- tests/algo_4.cpp
+++ tests/algo_4.cpp
@@ -16,39 +16,43 @@ int main()
 
     float d = sm::algo::dist_to_lineseg (t0, t1, p);
     std::cout << "Distance from " << p << " to " << t0 << "-" << t1 << " = " << d << std::endl;
-    if (d != 0.1) { --rtn; }
+    if (d != 0.1f) { std::cout << "Fail 0\n"; --rtn; }
 
     p = {0.5, 0.0001, 0.002};
     d = sm::algo::dist_to_lineseg (t0, t1, p);
+    std::cout.precision (12);
     std::cout << "Distance from " << p << " to " << t0 << "-" << t1 << " = " << d << std::endl;
-    if (d != 0.0020025) { --rtn; }
+    if (d != 0.00200249860063f) {
+        std::cout << "Fail 1\n";
+        --rtn;
+    }
 
 
     sm::vec<float> t2 = {0,1,0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     float d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
-    if (d != 0.0020025f || d2 != 4.01e-6f) { --rtn; }
+    if (d != 0.00200249860063f || d2 != 4.01000033889e-6f) { std::cout << "Fail 2\n";--rtn; }
 
     p = {0, 0.9, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
-    if (d != 0.0f || d2 != 0.0f) { --rtn; }
+    if (d != 0.0f || d2 != 0.0f) { std::cout << "Fail 3\n";--rtn; }
 
     p = {0, 0.8, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
-    if (d != 0.0f || d2 != 0.0f) { --rtn; }
+    if (d != 0.0f || d2 != 0.0f) { std::cout << "Fail 4\n";--rtn; }
 
     p = {0, 1.1, 0};
     d = sm::algo::dist_to_tri_edge (t0, t1, t2, p);
     d2 = sm::algo::dist_to_tri_edge_sq (t0, t1, t2, p);
     std::cout << "d to " << p << " = " << d << " sq: " << d2 << std::endl;
-    if (d != 0.1f || d2 != 0.01f) { --rtn; }
+    if (d != 0.100000023842f || d2 != 0.0100000044331f) { std::cout << "Fail 5\n";--rtn; }
 
-    std::cout << "Test " << (rtn ? "success" : "failure") << std::endl;
+    std::cout << "Test " << (rtn ? "FAIL" : "success") << std::endl;
 
     return rtn;
 }
