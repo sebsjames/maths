2096396e983d0b84186143821a02055bc88d802d
Seb James <sebj_coder3@pm.me>
2026-01-05 16:50:12 +0000

    A few more tests

--- tests/geometry_5.cpp
+++ tests/geometry_5.cpp
@@ -21,18 +21,35 @@ int main()
     v = { 0, 1, 20 };
     p = sm::geometry::vector_plane_projection (n, v);
     std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
-
     if (p != sm::vec<float>::uy()) { --rtn; }
 
     v = { 1, 0, 20 };
     p = sm::geometry::vector_plane_projection (n, v);
     std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
-
     if (p != sm::vec<float>::ux()) { --rtn; }
 
-    n = {0.3, 0.2, -0.5};
+    n = {0, -0.707, 0.707};
+    v = {0, 1, 1};
     p = sm::geometry::vector_plane_projection (n, v);
     std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
+    if (p != v) { --rtn; }
+
+    n = {0, 0.707, -0.707};
+    v = {0, 1, 1};
+    p = sm::geometry::vector_plane_projection (n, v);
+    std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
+    if (p != v) { --rtn; }
+
+    n = {0, 0.807, 0.707};
+    v = {0, 1, 1};
+    p = sm::geometry::vector_plane_projection (n, v);
+    std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
+
+    n = {0, 0.707, 0.707};
+    v = {0, 1, 1};
+    p = sm::geometry::vector_plane_projection (n, v);
+    std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
+    if (p != sm::vec<float>{}) { --rtn; }
 
     std::cout << "Test " << (rtn ? "FAIL" : "success") << std::endl;
 
