7a61d5f2ac7df1e94e6cfb0c0f570f0e7ac1d596
Seb James <sebj_coder3@pm.me>
2026-01-06 11:23:15 +0000

    vector_plane_projection works for arbitrary dimensions

--- tests/geometry_5.cpp
+++ tests/geometry_5.cpp
@@ -9,7 +9,7 @@ int main()
 {
     int rtn = 0;
 
-
+    // Projections of 3D vectors onto 2D planes
     sm::vec<float> n = {0, 0, 1};
     sm::vec<float> v = {0, 1, 1};
 
@@ -50,6 +50,13 @@ int main()
     p = sm::geometry::vector_plane_projection (n, v);
     std::cout << v << " projected onto the plane " << n << " is " << p << std::endl;
     if (p != sm::vec<float>{}) { --rtn; }
+
+    // Projection of a 2D vector onto a 1D line
+    sm::vec<float, 2> n2 = {0, 1};
+    sm::vec<float, 2> v2 = {1, 1};
+    sm::vec<float, 2> p2 = sm::geometry::vector_plane_projection (n2, v2);
+    std::cout << v2 << " projected onto the line/hyperplane defined by normal " << n2 << " is " << p2 << std::endl;
+    if (p2 != sm::vec<float, 2>::ux()) { --rtn; }
 
     std::cout << "Test " << (rtn ? "FAIL" : "success") << std::endl;
 
