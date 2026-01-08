84641bb9c99b5edb6e2e60dd557bc48caf0d4976
Seb James <sebj_coder3@pm.me>
2026-01-08 16:34:22 +0000

    Moves latlong_to_xyz alongside xyz_to_latlong and removes extraneous arg from xyz_to_latlong

--- tests/xyz_to_latlong.cpp
+++ tests/xyz_to_latlong.cpp
@@ -11,41 +11,41 @@ int main()
     sm::vec<float, 3> xyz = { 0, 0, 1 };
     xyz.renormalize();
     xyz *= r;
-    sm::vec<float, 2> ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    sm::vec<float, 2> ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
 
     xyz = { 0, 0, -1 };
     xyz.renormalize();
     xyz *= r;
-    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
 
     // x axis is at front of earth
     xyz = { 1, 0, 0 };
     xyz.renormalize();
     xyz *= r;
-    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
     if (ll[1] != 0) { std::cout << "Failed on +x\n"; --rtn; }
 
     xyz = { 0, 1, 0 };
     xyz.renormalize();
     xyz *= r;
-    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
     if (ll[1] != sm::mathconst<float>::pi_over_2) { std::cout << "Failed on +y axis\n"; --rtn; }
 
     xyz = { -1, 0, 0 };
     xyz.renormalize();
     xyz *= r;
-    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
     if (ll[1] != sm::mathconst<float>::pi) { std::cout << "Failed on -x axis\n"; --rtn; }
 
     xyz = { 0, -1, 0 };
     xyz.renormalize();
     xyz *= r;
-    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz, r);
+    ll = sm::geometry::spherical_projection::xyz_to_latlong (xyz);
     std::cout << xyz << " has Latitude "<< ll[0] << " and longitude " << ll[1] << std::endl;
     if (ll[1] != -sm::mathconst<float>::pi_over_2) { std::cout << "Failed on -y axis\n"; --rtn; }
 
