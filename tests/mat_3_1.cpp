1bb56170d0da3be94da69fb92a15cd7f918180f1
Seb James <sebj_coder3@pm.me>
2026-01-26 17:10:10 +0000

    Progress with the new, general sm::mat class

--- tests/mat_3_1.cpp
+++ tests/mat_3_1.cpp
@@ -6,15 +6,9 @@ int main()
 {
     int rtn = 0;
 
-    sm::mat<float, 4> m1 = { 0, 1, 0, 0,  1, 0, 0, 0,  0, 0, -1, 0,  0, 0, 0, 1 };
+    sm::mat<float, 3> m1 = { 0, 1, 0,  1, 0, 0,  0, 0, -1 };
 
     std::cout << "m1:\n" << m1 << std::endl;
-
-    sm::mat<float, 4> m2;
-
-    std::cout << "m2:\n" << m2 << std::endl;
-
-    std::cout << "m1 + m2:\n" << (m1 + m2) << std::endl;
 
     std::cout << "m1.rotation(): " << m1.rotation() << std::endl;
 
@@ -28,12 +22,6 @@ int main()
     } else {
         rtn = 1;
     }
-
-    sm::mat<float, 1, 3> onethree = { 1, 2, 3 };
-
-    sm::mat<float, 3, 2> threetwo = { 1, 2, 3, 4, 5, 6 };
-
-    std::cout << "mat mult: " << (onethree * threetwo) << std::endl;
 
     return rtn;
 }
