e61b32cd0ec77cb72a5ec27eb360323e2a91a725
Seb James <sebj_coder3@pm.me>
2026-03-23 11:14:06 +0000

    Adds test of has_size_method trait test

---
+++ tests/trait_tests_has_size.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <complex>
+#include <deque>
+#include <vector>
+
+import sm.trait_tests;
+import sm.vec;
+
+int main()
+{
+    int rtn = 0;
+
+    if constexpr (sm::has_size_method<sm::vec<float>>::value == true) {
+        std::cout << "vec<float> has size()\n";
+    } else {
+        std::cout << "vec<float> has NO size()\n";
+        --rtn;
+    }
+
+    if constexpr (sm::has_size_method<std::deque<int>>::value == true) {
+        std::cout << "deque<int> has size()\n";
+    } else {
+        std::cout << "deque<int> has NO size()\n";
+        --rtn;
+    }
+
+    if constexpr (sm::has_size_method<std::string>::value == true) {
+        std::cout << "std::string has size()\n";
+    } else {
+        std::cout << "std::string has NO size()\n";
+        --rtn;
+    }
+
+#if 0 // Fails to compile, rather than setting value false :(
+    if constexpr (sm::has_size_method<std::complex<float>>::value == true) {
+        std::cout << "std::complex<float> has size()\n";
+        --rtn;
+    } else {
+        std::cout << "std::complex<float> has NO size()\n";
+    }
+#endif
+    return rtn;
+}
