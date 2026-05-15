cb66297953084fd7cfd2d3f039625b2f731d4f88
Seb James <sebj_coder3@pm.me>
2026-05-15 15:55:55 +0100

    Adds code from an older branch ready to be hacked on, now that I can compute matrix eigenvectors

---
+++ tests/pca_iris.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+
+import sklearn.iris;
+import sm.vec;
+import sm.vvec;
+import sm.pca;
+
+int main()
+{
+    sm::vvec<sm::vec<double, 4>> x;
+    x.resize (sklearn::iris.size());
+    for (std::uint32_t i = 0; i < sklearn::iris.size(); ++i) {
+        x[i] = sklearn::iris[i].template as<double>();
+    }
+    [[maybe_unused]] sm::pca::result<double, 4> pca_res = sm::pca::compute<double, 4> (x);
+}
