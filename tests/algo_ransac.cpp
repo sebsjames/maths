6b707b074b673b4ab7baca6c68cd052408c386b3
Seb James <sebj_coder3@pm.me>
2026-06-18 10:35:58 +0100

    Add a randomised version of the algo to the test

--- tests/algo_ransac.cpp
+++ tests/algo_ransac.cpp
@@ -8,7 +8,7 @@
  * A 2D dataset is simulated composed of two parts; a linear part with 120 points and a Gaussian part, spatially distinct with 60 points
  * Standard linear regression will find  a line of best fit through all data points.
  * RANSAC should treat the Gaussian data points as outliers and return the linear best fit parameters for just the linear part of the data.
- * 
+ *
  * Author: Alex Blenkinsop
  * Date: June 2026
  */
@@ -62,16 +62,22 @@ int main()
 	sm::vec<float, 2> mc_linregr = sm::algo::linregr<std::vector, float>(x, y);
 	float m_linregr = mc_linregr[0];
 	float c_linregr = mc_linregr[1];
+	sm::vec<float, 2> mc_rnd = sm::algo::ransac<std::vector, float>(x, y, 300, 0.45f, 70);
+	float m_est_rnd = mc_rnd[0];
+	float c_est_rnd = mc_rnd[1];
 
 	std::cout << "True parameters: m=" << m_true << " c=" << c_true << std::endl;
 	std::cout << "Linear regression estimate: m=" << m_linregr << " c=" << c_linregr << std::endl;
-	std::cout << "RANSAC estimate: m=" << m_est << " c=" << c_est << std::endl;
+	std::cout << "RANSAC estimate (fixed seed): m=" << m_est << " c=" << c_est << std::endl;
+	std::cout << "RANSAC estimate (random seed): m=" << m_est_rnd << " c=" << c_est_rnd << std::endl;
 
 	// Use 5% relative tolerances based on the true parameter magnitudes.
 	const float m_tol = 0.05f * std::abs(m_true);
 	const float c_tol = 0.05f * std::abs(c_true);
 	if (std::abs(m_est - m_true) > m_tol) { --rtn; }
 	if (std::abs(c_est - c_true) > c_tol) { --rtn; }
+	if (std::abs(m_est_rnd - m_true) > m_tol) { --rtn; }
+	if (std::abs(c_est_rnd - c_true) > c_tol) { --rtn; }
 
 	std::cout << "Test " << (rtn < 0 ? "Failed" : "Passed") << std::endl;
 	return rtn;
