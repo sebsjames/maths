4a8910c815e35683741b6c52bf6bc9c420e55699
Seb James <sebj_coder3@pm.me>
2026-06-23 15:16:53 +0100

    Un-guards pca_iris, so that the issue with compilers can be demonstrated

--- tests/pca_iris.cpp
+++ tests/pca_iris.cpp
@@ -1,3 +1,8 @@
+/*
+ * Principle Component Analysis on some data from scikit-learn. See sklearn_iris.py for the a Python
+ * analysis.
+ */
+
 #include <iostream>
 #include <cstdint>
 
@@ -13,10 +18,10 @@ int main()
     for (std::uint32_t i = 0; i < sklearn::iris.size(); ++i) {
         x[i] = sklearn::iris[i].template as<double>();
     }
-    std::cout << "sm::pca::compute...\n";
-    [[maybe_unused]] sm::pca::result<double, 4> pca_res = sm::pca::compute<double, 4> (x);
+    std::cout << "Calling sm::pca::compute to obtain principle components...\n";
+    sm::pca::result<double, 4> pca_res = sm::pca::compute<double, 4> (x);
     for (std::uint32_t i = 0; i < 4; ++i) {
-        std::cout << "PC " << (i + 1) << " = " << pca_res.pc_ev_real[i] << " which accounts for " << pca_res.pc_mags[i] << " of the variability\n";
-        std::cout << "\n" << pca_res.x_proj[i] << "\n";
+        std::cout << "PC " << (i + 1) << " = " << pca_res.pc_ev_real[i]
+                  << " which accounts for " << pca_res.pc_mags[i] << " of the variability\n";
     }
 }
