2538ade9a690bf17be7e1c868e2ae4d117bd0948
Seb James <sebj_coder3@pm.me>
2026-06-24 16:58:25 +0100

    Makes the pca_iris test actually test

--- tests/pca_iris.cpp
+++ tests/pca_iris.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdint>
+#include <cmath>
 
 import sklearn.iris;
 import sm.vec;
@@ -23,4 +24,33 @@ int main()
     for (std::uint32_t i = 0; i < 4; ++i) {
         std::cout << "PC " << (i + 1) << " = " << pca_res.pc_vectors[i] << " which accounts for " << pca_res.pc_proportions[i] << " of the variability\n";
     }
+
+    int rtn = 0;
+    constexpr double eps = 0.00000001;
+
+    // These test values come from running sklearn_iris.py. First check the first principal components
+    sm::vec<double, 4> pc1_expected = { 0.36138659, -0.08452251, 0.85667061, 0.3582892 };
+    sm::vec<double, 4> diffs = (pc1_expected - pca_res.pc_vectors[0]).abs();
+    if (diffs > eps) { --rtn; }
+
+    // Now check all the explained variances
+    if (std::abs(pca_res.pc_proportions[0] - 0.92461872) > eps) {
+        std::cout << std::abs(pca_res.pc_proportions[0] - 0.92461872) << " is > " << eps << std::endl;
+        --rtn;
+    }
+    if (std::abs(pca_res.pc_proportions[1] - 0.05306648) > eps) {
+        std::cout << std::abs(pca_res.pc_proportions[1] - 0.05306648) << " is > " << eps << std::endl;
+        --rtn;
+    }
+    if (std::abs(pca_res.pc_proportions[2] - 0.01710261) > eps) {
+        std::cout << std::abs(pca_res.pc_proportions[2] - 0.01710261) << " is > " << eps << std::endl;
+        --rtn;
+    }
+    if (std::abs(pca_res.pc_proportions[3] - 0.00521218) > eps) {
+        std::cout << std::abs(pca_res.pc_proportions[3] - 0.00521218) << " is > " << eps << std::endl;
+        --rtn;
+    }
+
+    std::cout << "Test " << (rtn ? "FAILED" : "PASSED") << std::endl;
+    return rtn;
 }
